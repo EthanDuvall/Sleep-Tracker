7dd9aad9448d507103e8cce91d624ea2ac772498
Ethan <eduvall23@turing.edu>
2026-01-28 16:23:14 -0700

    getting somewhere give it time

---
+++ lib/screens/check_in_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CheckInScreen extends StatelessWidget {
+
+  final double stress;
+  final bool sleptWell;
+  final String note;
+
+
+  const CheckInScreen({
+    super.key,
+    required this.stress,
+    required this.sleptWell,
+    required this.note,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
